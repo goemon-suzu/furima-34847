33b22d33afb7377255460583503e7a3e7f215267
goemon-suzu <chihiro.y525@gmail.com>
2021-04-01 16:42:17 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,24 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+#  has_one :order
+
+  with_options presence: true do
+    with_options numericality: { other_than: 0 } do
+      validates :item_name
+      validates :description
+      validates :category_id
+      validates :condition_id
+      validates :delivery_setting_id
+      validates :shipment_area_id
+      validates :shipping_days_id
+      
+      with_options in: 300..9_999_999, message: 'This site is only for under 300 and over 9,999,999'
+        validates :price
+      end
+    end
+  end
+
 
 
 
