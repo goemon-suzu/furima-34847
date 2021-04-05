46cd49af6a1b71a103a8e49148db6201f5704317
goemon-suzu <chihiro.y525@gmail.com>
2021-04-05 17:03:55 +0900

    戻るリンクの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_to_show, only:[:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -39,5 +40,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def redirect_to_show
+    return redirect_to root_path if current_user.id != @item.user.id
   end
 end
