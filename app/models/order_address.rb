b043fce8ad31ca007bd1fbe3fc6c026f915127e4
goemon-suzu <chihiro.y525@gmail.com>
2021-04-08 00:38:57 +0900

    トークンに対するバリデーションを設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :city, :street_address
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :phone_number, format: { with: /\A\d{10,11}\z/}
+    validates :token
   end
 
 
