8ad298816aea6ee918f8a19f0d9bce7cc6179893
goemon-suzu <chihiro.y525@gmail.com>
2021-04-08 15:53:12 +0900

    バリデーションの追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,7 +7,7 @@ class OrderAddress
     validates :street_address
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 0 }
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
     validates :token
     validates :user_id
     validates :item_id
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OrderAddress, type: :model do
     user = FactoryBot.create(:user)
     item = FactoryBot.create(:item)
     @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
-    sleep 1.5
+    sleep 2
   end
 
   describe '商品購入機能' do
@@ -61,6 +61,12 @@ RSpec.describe OrderAddress, type: :model do
       end
       it '電話番号は11桁以内であること' do
         @order_address.phone_number = '123056788888888'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+
+      it '電話番号は英数混合では登録できないこと' do
+        @order_address.phone_number = '090abcd1234'
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
