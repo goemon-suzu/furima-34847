fc40e7d5c5b19e59da67b7290dca33ae4ecf3100
goemon-suzu <chihiro.y525@gmail.com>
2021-04-14 23:07:02 +0900

    テストコード修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,7 @@ class OrderAddress
     validates :city
     validates :street_address
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :prefecture_id, numericality: { other_than: 0 , message: "を選択してください。"}
     validates :phone_number, format: { with: /\A\d{11}\z/ }
     validates :token
     validates :user_id
